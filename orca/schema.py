e06d75876f3b19e6aa38fbc160968d41e5f2248d
Adam <aashelt90@gmail.com>
2019-01-21 12:03:28 -0500

    update schema

--- orca/schema.py
+++ orca/schema.py
@@ -1,13 +1,53 @@
 schema = {
     "$schema": "https://json-schema.org/schema#",
+    "id": "orca_schema_v1",
     "type": "object",
+    "required": ["version"],
+
     "properties": {
-        "test" : {
-            "type": "object",
-            "properties" :{
-                "apples": {"type" : "number"},
-                "bananas": {"type": "number"}
-            }
+        "version": { "type": "string"},
+        "id": "#/properties/steps",
+        "steps" : {
+            "type": "array",
+            "items" : "#definitions/step"
         }
-    }
+
+    },
+
+    "definitions": {
+        "step": {
+            "id": "#/definitions/step",
+            "type": "object",
+            "properties": {
+                "config": {
+                    "type": "array",
+                    "items": "#definitions/configItem" 
+                },
+                "service":{
+                    "type": "object",
+                    "properties":{
+                        "host": {"type": "string"},
+                        "port": {"type": "string"},
+                        "name": {"type": "string"}
+                    }
+                }
+            }
+        },
+
+        "configItem": {
+            "id": "#/definitions/configItem",
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "value": {"type": ["string", "number", "object", "array"]}
+            },
+        }
+    },
+
+    # "constraints": {
+    #     "configItem": {
+    #         "id": "#/definitions/constraints/configItem",
+    #         "any"
+    #     }
+    # }
 }
