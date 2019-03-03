d070cdbd49aa109148114d8de1a0a681cffe5681
Olaf David <odavid@colostate.edu>
2019-03-02 17:05:19 -0700

    cleanup

--- orca/core/ledger.py
+++ orca/core/ledger.py
@@ -14,24 +14,26 @@ class Ledger(object):
 
   # unique for each orca run
   _id = uuid.uuid4()
+  
+  def set_config(self, config: OrcaConfig):
+    self.config = config
 
-  def add(self, config: OrcaConfig, task: OrcaTask, inputs:Dict, outputs:Dict) -> None:
+  def add(self, task: OrcaTask, state:Dict) -> None:
     """Add an entry to the ledger"""
-    print("->> ledger: {0} {1} {2} {3} ".format(task.name, str(self._id), str(inputs), str(outputs)))
-    pass
+    print("->> ledger: {0} {1} {2}".format(task.name, str(self._id), str(state)))
+    #pass
 
-  def _create_entry(self, config: OrcaConfig, task: OrcaTask, inputs:Dict, outputs:Dict) -> Dict:
+  def _create_entry(self, task: OrcaTask, state: Dict) -> Dict:
     """Create a dictionary entry to record in a ledger"""
     d = {
-          'orca_file': os.path.abspath(config.get_yaml_file()), # the workflow file
-          'orca_id': config.get_version(), # the 'version' entry in the workflow file
-          'orca_name': config.get_name(), # the 'version' entry in the workflow file (e.g. gitattribute)
+          'orca_file': os.path.abspath(self.config.get_yaml_file()), # the workflow file
+          'orca_id': self.config.get_version(), # the 'version' entry in the workflow file
+          'orca_name': self.config.get_name(), # the 'version' entry in the workflow file (e.g. gitattribute)
           'run_uuid': str(self._id), # the uuid of the current run 
           'run_time': str(datetime.now()), # task execution time
           'task': task.name # the task name
         }
-    d.update(inputs)  # task inputs/outputs
-    d.update(outputs)
+    d.update(state)  # task inputs/outputs
     if log.isEnabledFor(logging.DEBUG):
       log.debug('ledger: {0}'.format(d))
     return d
@@ -41,21 +43,18 @@ class Ledger(object):
 
 
 ############################################
-# 
-# python3 orca run --json-ledger-file /tmp/f.json for.yaml
-#
-# maybe import into a db:
-# mongoimport --db orca --collection ledger --file /tmp/f.json
+
 
 class JSONFileLedger(Ledger):
+  """Creates a JSON File with a ledger, appends if file exists."""
   
   def __init__(self, file: str):
     self.f = open(file, 'a+')
     log.debug('JSON Ledger: {0}'.format(self.f))
 
   
-  def add(self, config: OrcaConfig, task: OrcaTask, inputs:Dict, outputs:Dict) -> None:
-    d = self._create_entry(config, task, inputs, outputs)
+  def add(self, task: OrcaTask, state:Dict) -> None:
+    d = self._create_entry(task, state)
     self.f.write('{0}\n'.format(d))
 
     
@@ -65,25 +64,23 @@ class JSONFileLedger(Ledger):
     
     
 ##################################################################    
-# python3 orca run --json-ledger-file /tmp/f.json for.yaml
 
 class MongoLedger(Ledger):
+  """Adds entries into a ledger hosted in Mongodb"""
     
-  def __init__(self, connect_str: str):
-    mc = connect_str.split('/')
-    self.c = pymongo.MongoClient('mongodb://{0}/'.format(mc[0]))
-    db = self.c[mc[1]]
-    self.col = db[mc[2]]
-    log.debug('Mongo Ledger: {0} for {1}'.format(self.c, connect_str))
+  def __init__(self, con: List[str]):
+    super().__init__()
+    self.mongo = pymongo.MongoClient('mongodb://{0}/'.format(con[0]))
+    self.col = self.mongo[con[1]][con[2]]
+    log.debug('Mongo Ledger: {0} for {1}'.format(self.mongo, "/".join(con)))
 
   
-  def add(self, config: OrcaConfig, task: OrcaTask, inputs:Dict, outputs:Dict) -> None:
-    d = self._create_entry(config, task, inputs, outputs)
+  def add(self, task: OrcaTask, state:Dict) -> None:
+    d = self._create_entry(task, state)
     self.col.insert_one(d)
 
     
   def close(self) -> None:
-    self.c.close()
-    log.debug('closed: {0}'.format(self.c))
-    
+    self.mongo.close()
+    log.debug('closed: {0}'.format(self.mongo))
     
